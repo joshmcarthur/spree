6124959385b802079ac66c65e04838c11ca18c56
David North <davidnorth@railsdog.com>
2010-09-09 12:39:17 -0500

    Now able to login

--- auth/app/models/user.rb
+++ auth/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   belongs_to :bill_address, :foreign_key => "bill_address_id", :class_name => "Address"
 
   before_save :check_admin
+  before_validation :set_login
 
   acts_as_authentic do |c|
     c.transition_from_restful_authentication = true
@@ -20,7 +21,6 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :anonymous
-  after_save :ensure_authentication_token!
 
   alias_attribute :token, :authentication_token
 
@@ -40,10 +40,17 @@ class User < ActiveRecord::Base
   end
 
   private
+
   def check_admin
     if User.where("roles.name" => "admin").includes(:roles).empty?
       self.roles << Role.find_by_name("admin")
     end
     true
   end
+
+  def set_login
+    # for now force login to be same as email, eventually we will make this configurable, etc.
+    self.login ||= self.email if self.email
+  end 
+
 end
